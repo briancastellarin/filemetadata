d723418a31a7e91a3dfed94a5c8b0ed228191586
thizk <briancastellarin@gmail.com>
2022-03-15 20:05:38 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var multer = require('multer');
+var cors = require('cors');
+var app = express();
+require('dotenv').config()
+
+// DESTINATION
+const upload = multer({ dest: './upload' });
+
+//
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+//
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+//
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
+
+//
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
